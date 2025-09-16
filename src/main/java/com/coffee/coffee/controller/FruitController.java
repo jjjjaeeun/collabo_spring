61ba012274e1803387e548c4ee842268f56247ab
jj <bluesky@example.com>
2025-09-16 17:06:36 +0900

    25.09.16

---
+++ src/main/java/com/coffee/coffee/controller/FruitController.java
@@ -0,0 +1,29 @@
+package com.coffee.coffee.controller;
+
+import com.coffee.coffee.entity.Fruit;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController // 컨트롤러는 특정 요청에 대한 처리를 수행
+public class FruitController {
+    @GetMapping("/fruit")
+    public Fruit test (){
+        Fruit bean = new Fruit();
+        bean.setId("banana");
+        bean.setName("바나나");
+        bean.setPrice(1000);
+        return bean ;
+    }
+
+    @GetMapping("/fruit/list")
+    public List<Fruit> test02(){
+        List<Fruit> fruitList = new ArrayList<>();
+        fruitList.add(new Fruit("apple","사과", 1000));
+        fruitList.add(new Fruit("pear","배", 2000));
+        fruitList.add(new Fruit("grape","포도", 3000));
+        return fruitList;
+    }
+}
